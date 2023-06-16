90635db26084af98de7b59317d6aab049bd8c865
MedamineToubib <medamine.toubib@gmail.com>
2023-06-15 18:58:03 -0700

    6-size.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 /**
-* main - program that use printf function to print a line 
+* main - Entry point
+* Description:  program that use printf function to print a line 
 * Return: Always 0 (success)
 */
+
 int main(void)
 {
-Printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+
+	Printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
